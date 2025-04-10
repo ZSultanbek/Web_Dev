e0d8321b6dc021fadeddac7a1063441f90833428
Humpty Dumpty <zhanibek220506@gmail.com>
2025-04-10 16:27:36 +0500

    lab9 done

---
+++ lab9/hh_back/api/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField(max_length=200)
+    city = models.CharField(max_length=20)
+    address = models.TextField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+    
+class Vacancy(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField(max_length=200)
+    salary = models.FloatField()
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
